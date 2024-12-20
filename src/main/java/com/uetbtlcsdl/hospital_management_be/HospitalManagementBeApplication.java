7def87b25ac1c23bd96b4a3293f3cd29ed8e689c
CB303 <quachthanhhung12349@gmail.com>
2024-12-20 22:49:25 +0700

    Initial commit

---
+++ src/main/java/com/uetbtlcsdl/hospital_management_be/HospitalManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.uetbtlcsdl.hospital_management_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uetbtlcsdl/hospital_management_be/HospitalManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uetbtlcsdl.hospital_management_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
