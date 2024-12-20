b86388dde4544d2374816aea096ea6761582ded4
Vu Huy Dong <huydongvu0105@gmail.com>
2024-12-21 00:19:28 +0700

    aa

--- src/main/java/com/uetbtlcsdl/hospital_management_be/configuration/SwaggerConfig.java
+++ src/main/java/com/uetbtlcsdl/hospital_management_be/configuration/SwaggerConfig.java
@@ -1,4 +1,41 @@
 package com.uetbtlcsdl.hospital_management_be.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
 public class SwaggerConfig {
+    /**
+     * @program: project2
+     * @description:
+     * @author: Luke
+     * @create: 2021-01-07 16:10
+     **/
+
+
+package com.revature.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+    @Configuration
+    public class SwaggerConfig  implements WebMvcConfigurer {
+
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+            registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+            registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+            registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+        }
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+            registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+
+
+
+
 }
